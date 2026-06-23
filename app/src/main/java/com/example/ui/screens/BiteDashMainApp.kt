bb9c2de1319af45c0b967f58def86752dfcae5f2
fingerprintacoustic <fingerprintacoustic@gmail.com>
2026-06-22 21:35:23 -0500

    Update BiteDashMainApp.kt

--- app/src/main/java/com/example/ui/screens/BiteDashMainApp.kt
+++ app/src/main/java/com/example/ui/screens/BiteDashMainApp.kt
@@ -2402,7 +2402,7 @@ fun AdminPortalOverlay(
                                 }
                             }
                         }
-                        }
+                        }}
                         3 -> {
                             // Paynow Credentials
                             PaynowSettingsTab(viewModel = viewModel)
