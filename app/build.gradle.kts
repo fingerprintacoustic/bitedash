5ad4a5e434bcacfa056c61199af476d3acbfdee5
Joseph <joseph@fingerprintacoustic.com>
2026-08-05 01:23:46 +0000

    Bump version to 6.4 (versionCode 10) for simulator removal release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.aistudio.bitedash.znvtxp"
     minSdk = 24
     targetSdk = 36
-    versionCode = 9
-    versionName = "6.3"
+    versionCode = 10
+    versionName = "6.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
